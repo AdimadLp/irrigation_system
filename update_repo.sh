1b86953406ea5df902b32f88f916523cd65846a2
AdimadLp <adimadlp@outlook.com>
2024-06-11 15:15:00 +0200

    Refactor update_repo.sh to use absolute paths for git commands

--- update_repo.sh
+++ update_repo.sh
@@ -10,8 +10,8 @@ has_repo_changed() {
 
 update_repo() {
     # Pull updates from the remote repository
-    /usr/bin/git -C %h/localrepo reset --hard main
-    /usr/bin/git -C %h/localrepo pull
+    /usr/bin/git -C irrigation_system reset --hard main
+    /usr/bin/git -C irrigation_system pull
 }
 
 check_and_update_repo() {
