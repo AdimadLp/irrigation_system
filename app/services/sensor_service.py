b3adca0573c24a6ee7f929b7ce2b34ffda52aefc
AdimadLp <adimadlp@outlook.com>
2024-09-10 00:46:24 +0200

    test

--- app/services/sensor_service.py
+++ app/services/sensor_service.py
@@ -37,7 +37,7 @@ class SensorService:
         while not self.stop_event.is_set():
             try:
                 new_data = await self.read_sensor_data()
-                await asyncio.sleep(1)  # Read data every second
+                await asyncio.sleep(10)  # Read data every second
 
                 if new_data:  # Only push if there's data
                     for data in new_data:
