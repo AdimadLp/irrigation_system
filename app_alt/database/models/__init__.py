dbe15e2ec26311f207c2c4991cc3871aa32bfb07
adimadlp <adimadlp@outlook.com>
2024-12-18 12:03:57 +0100

    Remove app_alt

--- app_alt/database/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .plants import Plants
-from .irrigation_controllers import IrrigationControllers
-from .sensors import Sensors
-from .pumps import Pumps
-from .schedules import Schedules
-from .watering_logs import WateringLogs
-from .realtime_sensor_data import RealtimeSensorData
